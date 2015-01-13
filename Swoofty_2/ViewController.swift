a99ea7a2a8f6a7827d16dd85b9dbb26f4b05ae12
Daniel Creagh <daniel@creagh.com>
2015-01-13 16:48:05 +0000

    Initial Commit

---
+++ Swoofty_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swoofty_2
+//
+//  Created by Daniel Creagh on 13/01/2015.
+//  Copyright (c) 2015 Daniel Creagh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
